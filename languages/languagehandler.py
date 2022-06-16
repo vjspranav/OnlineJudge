e3e40f6d4627f04b63739d43e3ecce2c3706b152
vjspranav <pranavasri@live.in>
2022-06-16 15:53:21 +0530

    Add a skeleton for language handler

---
+++ languages/languagehandler.py
@@ -0,0 +1,66 @@
+class LanguageHandler:
+    def __init__(self, language, input_files, output_files, user_input):
+        self.language = language
+        self.input_files = input_files
+        self.output_files = output_files
+        self.user_input = user_input or None
+
+    # Add a function which enforces execution of the language handler
+    def execute(self):
+        if self.user_input:
+            self.execute_single()
+        else:
+            self.execute_batch()
+        pass
+
+
+# Inherit from LanguageHandler for python
+class PythonHandler(LanguageHandler):
+    def __init__(self, language, input_files, output_files, user_input):
+        super().__init__(language, input_files, output_files, user_input)
+
+    def execute(self):
+        super().execute()
+
+    def execute_single(self):
+        # Execute the language handler
+        print("Executing Single Python Handler")
+        pass
+
+    def execute_batch(self):
+        print("Executing Batch Python Handler")
+        pass
+
+
+# Inherit from LanguageHandler for C
+class CHandler(LanguageHandler):
+    def __init__(self, language, input_files, output_files, user_input):
+        super().__init__(language, input_files, output_files, user_input)
+
+    def execute(self):
+        super().execute()
+
+    def execute_single(self):
+        # Execute the language handler
+        pass
+
+    def execute_batch(self):
+        # Execute the language handler
+        pass
+
+
+# Inherit from LanguageHandler for C++
+class CppHandler(LanguageHandler):
+    def __init__(self, language, input_files, output_files, user_input):
+        super().__init__(language, input_files, output_files, user_input)
+
+    def execute(self):
+        super().execute()
+
+    def execute_single(self):
+        # Execute the language handler
+        pass
+
+    def execute_batch(self):
+        # Execute the language handler
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Body
+from languages.languagehandler import PythonHandler, CHandler, CppHandler
 
 app = FastAPI()
 
@@ -11,3 +12,20 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/execute")
+async def language_handler(request: Request):
+    request_json = await request.json()
+    language = request_json["language"]
+    input_files = request_json["input_files"]
+    output_files = request_json["output_files"]
+    user_input = request_json["user_input"]
+
+    if language == "python":
+        handler = PythonHandler(language, input_files, output_files, user_input)
+    elif language == "c":
+        handler = CHandler(language, input_files, output_files, user_input)
+
+    handler.execute()
+    return {"message": "Executed"}
